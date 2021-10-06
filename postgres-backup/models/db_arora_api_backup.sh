8cf3abb5e76663728b34bc610ec5950cbb1b45b9
guidojw <guidojwdejong@hotmail.com>
2021-10-07 00:39:36 +0200

    fix(backups): correctly delete old backups

--- postgres-backup/models/db_arora_api_backup.sh
+++ postgres-backup/models/db_arora_api_backup.sh
@@ -7,11 +7,9 @@ DATE=$(date +%Y.%m.%d.%H.%M.%S)
 echo "Dumping $1 to $BACKUP_LOCATION/$1/$DATE/$1.tar.enc"
 export PGPASSWORD=$POSTGRES_PASSWORD
 mkdir -p "$BACKUP_LOCATION"/"$1"/"$DATE"
-pg_dump -Ft --username="$POSTGRES_USER" -h db "$POSTGRES_DATABASE" | gzip | openssl aes-256-cbc -base64 -md sha256 \
-  -pass file:password -salt \
-  -out "$BACKUP_LOCATION"/"$1"/"$DATE"/"$1".tar.enc
+pg_dump -Ft --username="$POSTGRES_USER" -h db "$POSTGRES_DATABASE" | gzip | \
+  openssl aes-256-cbc -base64 -md sha256 -pass file:password -salt -out "$BACKUP_LOCATION"/"$1"/"$DATE"/"$1".tar.enc
 
-find "$BACKUP_LOCATION" -type f -prune -mtime \
-  +$KEEP -exec rm -drf {} \;
+find "$BACKUP_LOCATION" -mindepth 1 -maxdepth 1 -mtime +$KEEP -exec rm -rf {} +
 
 bash notifiers/discord.sh "$1"
