f2d0e35db3f4c10523c419cc5e27b6b117e3a403
guidojw <35309288+guidojw@users.noreply.github.com>
2026-05-19 00:15:05 +0200

    fix(Traefik): trust Docker cloudflared network

--- traefik/bin/update-certs.sh
+++ traefik/bin/update-certs.sh
@@ -2,5 +2,5 @@
 kubectl get secret -n traefik legacy-cert -o jsonpath="{.data['tls\.crt']}" | base64 --decode > tls.crt
 kubectl get secret -n traefik legacy-cert -o jsonpath="{.data['tls\.key']}" | base64 --decode > tls.key
 
-scp tls.crt guido@192.168.1.55:/opt/docker/traefik/dynamic/tls.crt
-scp tls.key guido@192.168.1.55:/opt/docker/traefik/dynamic/tls.key
+scp tls.crt guido@192.168.1.20:/opt/docker/traefik/dynamic/tls.crt
+scp tls.key guido@192.168.1.20:/opt/docker/traefik/dynamic/tls.key
